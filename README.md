f5b5e491a8c41939df1fb7492d4d848580f32160
Kunal Sharma <kunal0199@gmail.com>
2023-08-14 11:23:42 +0530

    project and environment setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Note Taking App
+This app allows you to create notebooks and use it for taking notes digitally.
+
+*This project is hands on practice for learning REST development in go lang.*
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Kunal-Patro/NoteTakingApp/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDatabase()
+}
+
+func main() {
+	fmt.Println("Getting Started...")
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "OK",
+		})
+	})
+
+	router.Run()
+}
