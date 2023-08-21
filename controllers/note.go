282ee892cc9c1f09e8b776322e391b469149ef42
Kunal Sharma <kunal0199@gmail.com>
2023-08-21 14:51:25 +0530

    move get all notes api to service layer

--- controllers/note.go
+++ controllers/note.go
@@ -1,17 +1,12 @@
 package controllers
 
 import (
-	"fmt"
-	"math"
 	"net/http"
-	"strconv"
 
 	"github.com/Kunal-Patro/NoteTakingApp/dto"
-	"github.com/Kunal-Patro/NoteTakingApp/initializers"
 	"github.com/Kunal-Patro/NoteTakingApp/models"
 	"github.com/Kunal-Patro/NoteTakingApp/services"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 )
 
 const NOTES_PER_PAGE = 3
@@ -45,83 +40,18 @@ func CreateNote(c *gin.Context) {
 func GetAllNotes(c *gin.Context) {
 	user, _ := c.Get("user")
 	notebookID := c.Param("notebook_id")
-
-	var notebook models.Notebook
-	initializers.DB.Find(&notebook, "id = ? AND user_id = ?", notebookID, user.(models.User).ID)
-
-	if notebook.ID == uuid.Nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Cannot find the notebook.",
-		})
-		return
-	}
-
 	pageStr := c.DefaultQuery("page", "1")
-	page, err := strconv.Atoi(pageStr)
 
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Unable to fetch page.",
-		})
-		return
+	res := services.FetchAllNotes(notebookID, pageStr, user.(models.User))
+
+	tag := "notes"
+	if res.Code != http.StatusOK {
+		tag = "error"
 	}
 
-	var notesCount int64
-	if err := initializers.DB.Table("notes").Count(&notesCount).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to count notes.",
-		})
-		return
-	}
-
-	pageCount := int(math.Ceil(float64(notesCount) / float64(NOTES_PER_PAGE)))
-
-	if pageCount == 0 {
-		pageCount = 1
-	}
-
-	if page < 1 || page > pageCount {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "invalid page",
-		})
-		return
-	}
-
-	offset := (page - 1) * NOTES_PER_PAGE
-
-	var notes []models.Note
-	result := initializers.DB.Limit(NOTES_PER_PAGE).Offset(offset).
-		Find(&notes, "notebook_id = ?", notebookID)
-
-	if result.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to fetch notes.",
-		})
-		return
-	}
-
-	var prevPage, nextPage string
-
-	if page > 1 {
-		prevPage = fmt.Sprintf("%d", page-1)
-	}
-	if page < pageCount {
-		nextPage = fmt.Sprintf("%d", page+1)
-	}
-
-	pages := make([]int, pageCount)
-
-	for i := range pages {
-		pages[i] = i + 1
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"notes":      notes,
-		"next_page":  nextPage,
-		"prev_page":  prevPage,
-		"page_count": pageCount,
-		"page":       page,
-		"pages":      pages,
+	c.JSON(res.Code, gin.H{
+		tag:         res.Body,
+		"page_data": res.Page, // page data will be {"page_count": 0,"curr_page": 0,"prev_page": "","next_page": "","pages": null} in case of error.
 	})
 }
 
--- services/note.go
+++ services/note.go
@@ -1,7 +1,10 @@
 package services
 
 import (
+	"fmt"
+	"math"
 	"net/http"
+	"strconv"
 
 	"github.com/Kunal-Patro/NoteTakingApp/dto"
 	"github.com/Kunal-Patro/NoteTakingApp/initializers"
@@ -39,6 +42,89 @@ func CreateNewNote(body *dto.NoteDTO, notebookID string, user models.User) types
 	return types.Response{
 		Code: http.StatusOK,
 		Body: "New note created.",
+	}
+}
+
+func FetchAllNotes(notebookID string, pageStr string, user models.User) types.Response {
+	const NOTES_PER_PAGE = 3
+	var notebook models.Notebook
+	initializers.DB.Find(&notebook, "id = ? AND user_id = ?", notebookID, user.ID)
+
+	if notebook.ID == uuid.Nil {
+		return types.Response{
+			Code: http.StatusBadRequest,
+			Body: "Cannot find the notebook.",
+		}
+	}
+
+	page, err := strconv.Atoi(pageStr)
+
+	if err != nil {
+		return types.Response{
+			Code: http.StatusBadRequest,
+			Body: "Unable to fetch page.",
+		}
+	}
+
+	var notesCount int64
+	if err := initializers.DB.Table("notes").Count(&notesCount).Error; err != nil {
+		return types.Response{
+			Code: http.StatusInternalServerError,
+			Body: "Failed to count notes",
+		}
+	}
+
+	pageCount := int(math.Ceil(float64(notesCount) / float64(NOTES_PER_PAGE)))
+
+	if pageCount == 0 {
+		pageCount = 1
+	}
+
+	if page < 1 || page > pageCount {
+		return types.Response{
+			Code: http.StatusBadRequest,
+			Body: "Invalid page.",
+		}
+	}
+
+	offset := (page - 1) * NOTES_PER_PAGE
+
+	var notes []models.Note
+	result := initializers.DB.Limit(NOTES_PER_PAGE).Offset(offset).
+		Find(&notes, "notebook_id = ?", notebookID)
+
+	if result.Error != nil {
+		return types.Response{
+			Code: http.StatusInternalServerError,
+			Body: "Failed to fetch notes.",
+		}
+	}
+
+	var prevPage, nextPage string
+
+	if page > 1 {
+		prevPage = fmt.Sprintf("%d", page-1)
+	}
+	if page < pageCount {
+		nextPage = fmt.Sprintf("%d", page+1)
+	}
+
+	pages := make([]int, pageCount)
+
+	for i := range pages {
+		pages[i] = i + 1
+	}
+
+	return types.Response{
+		Code: http.StatusOK,
+		Body: notes,
+		Page: types.PaginatedData{
+			PageCount: pageCount,
+			CurrPage:  page,
+			NextPage:  nextPage,
+			PrevPage:  prevPage,
+			Pages:     pages,
+		},
 	}
 }
 
